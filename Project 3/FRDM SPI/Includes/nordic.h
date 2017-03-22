25bc6743c9c9f02516a16b2d2a9043585711b743
priyankapashte <priyankapashte@gmail.com>
2017-03-22 15:45:42 -0600

    spi.h created

---
+++ Project 3/FRDM SPI/Includes/nordic.h
@@ -0,0 +1,15 @@
+/*
+ * nordic.h
+ *
+ *  Created on: Mar 22, 2017
+ *      Author: Priyanka
+ */
+
+#ifndef INCLUDES_NORDIC_H_
+#define INCLUDES_NORDIC_H_
+
+
+
+
+
+#endif /* INCLUDES_NORDIC_H_ */
---
+++ Project 3/FRDM SPI/Includes/spi.h
@@ -0,0 +1,60 @@
+/*
+ * spi.h
+ *
+ *  Created on: Mar 22, 2017
+ *      Author: Priyanka
+ */
+
+#ifndef INCLUDES_SPI_H_
+#define INCLUDES_SPI_H_
+
+
+/**************************************************************************************
+* SPI_init
+* Initializes the SPI controller
+*
+* @param size - None
+* @return - None
+**************************************************************************************/
+ void SPI_init();
+
+ /**************************************************************************************
+ * SPI_read_byte
+ * Reads a single byte from the SPI bus
+ *
+ * @param byte - Store the value read in byte
+ * @return - None
+ **************************************************************************************/
+void SPI_read_byte(uint8_t byte);
+
+/**************************************************************************************
+* SPI_write_byte
+* Sends a single byte on the SPI bus
+*
+* @param byte - Value of the byte to be written
+* @return - None
+**************************************************************************************/
+void SPI_write_byte(uint8_t byte);
+
+
+/**************************************************************************************
+* SPI_send_packet
+* Sends numerous SPI Bytes given a pointer to a byte array and a length of how many
+* bytes to send.
+*
+* @param p - Pointer to the packet start
+* @param size - Size of the packet
+* @return - None
+**************************************************************************************/
+void SPI_send_packet(uint8_* p, size_t length);
+
+
+/**************************************************************************************
+* SPI_flush
+* Blocks until SPI transmit buffer has completed transmitting
+*
+* @return - None
+**************************************************************************************/
+void SPI_flush();
+
+#endif /* INCLUDES_SPI_H_ */
